f4ce1871a8e112e9d64dacf2f42be3b4aad85767
joel-a4817 <joel.ag789@gmail.com>
2025-12-14 07:35:08 +0000

    Add OpenCV to home.nix package list

--- home.nix
+++ home.nix
@@ -11,12 +11,13 @@
   # Packages for your setup
   home.packages = with pkgs; [
     prismlauncher #https://wiki.nixos.org/wiki/Prism_Launcher
-    signal-desktop #no official 
+    signal-desktop #no official for nix
     vscode #https://wiki.nixos.org/wiki/Visual_Studio_Code 
     gitkraken #snap-store official: https://snapcraft.io/gitkraken
     kicad #flatpak official: https://www.kicad.org/download/linux/
     prusa-slicer #appimage official: https://prusaslicer.net/#download 
     bambu-studio #flatpak official: https://flathub.org/en/apps/com.bambulab.BambuStudio
+    opencv #git official: https://docs.opencv.org/4.12.0/d7/d9f/tutorial_linux_install.html
   ];
 
   # Foot terminal config
