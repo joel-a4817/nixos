e1dfc2e364362d3e26233497154b4d2f0d8532f4
joel-a4817 <joel.ag789@gmail.com>
2025-12-14 01:22:02 +0000

    Add Solaar input for device management
    Added Solaar input to flake.nix for device management.

--- flake.nix
+++ flake.nix
@@ -1,18 +1,22 @@
-
 {
-  description = "Joel's NixOS + Sway setup";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     home-manager.url = "github:nix-community/home-manager/release-25.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    solaar = {
+      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
+      #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.6.tar.gz"; # uncomment line for solaar version 1.1.18
+      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, home-manager, solaar }:
     {
       nixosConfigurations.rt4817 = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
+          solaar.nixosModules.default
           ./configuration.nix
 
           # Home Manager as a NixOS module
