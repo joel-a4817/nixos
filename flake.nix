e7c0239edf77a83b861d5cded74697760e620421
rt4817 <joel.ag789@gmail.com>
2025-12-16 09:09:01 +0800

    Error fixes for solaar, switched to unstable

--- flake.nix
+++ flake.nix
@@ -17,11 +17,10 @@
       flake = false;
     };
     
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     solaar = {
-      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
+      #url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
       #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.6.tar.gz"; # uncomment line for solaar version 1.1.18
-      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
