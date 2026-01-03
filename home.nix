a379cc5b404d1af0d91602eb886761f3f1d87db9
rt4817 <joel.ag789@gmail.com>
2026-01-03 09:11:38 +0530

    Switched to vim
    neovim has some features that i don't use. vim is more minimal, so imma
    js use that :).

--- home.nix
+++ home.nix
@@ -24,8 +24,7 @@
     qt5.qtwayland #Required for Qt apps like those above.
     #yazi pkgs:
     ffmpeg p7zip jq poppler fzf zoxide resvg imagemagick
-    trash-cli jdupes lazygit #required by yazi plugins
-    fd ripgrep #required by neovim (telescope) and yazi.
+    trash-cli jdupes lazygit fd ripgrep #required by yazi plugins
   ];
 
 programs.foot = {
@@ -35,7 +34,7 @@ programs.foot = {
       font = "JetBrainsMono Nerd Font:size=16";
     };
     colors = {
-      alpha = 0.88;
+      alpha = 0.90;
       background = "1d1c22";
       foreground = "ffffff";
     };
@@ -54,66 +53,6 @@ programs.foot = {
       restore     = pkgs.yaziPlugins.restore; #https://github.com/boydaihungst/restore.yazi
     };
   };
-
-programs.neovim = {
-  enable = true;
-  defaultEditor = true;
-  viAlias = true;
-  vimAlias = true;
-
-  plugins = with pkgs.vimPlugins; [
-    telescope-nvim
-    plenary-nvim
-    telescope-fzf-native-nvim
-  ];
-
-  extraLuaConfig = ''
-    vim.g.mapleader = " "
-
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.opt.termguicolors = true
-    vim.opt.expandtab = true
-    vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
-    vim.opt.signcolumn = "yes"
-
-    vim.cmd [[
-      hi Normal guibg=NONE ctermbg=NONE
-      hi NormalNC guibg=NONE ctermbg=NONE
-      hi NonText guibg=NONE ctermbg=NONE
-    ]]
-
-    require("telescope").setup({
-      extensions = {
-        fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = "smart_case",
-        },
-      },
-    })
-    require("telescope").load_extension("fzf")
-
-    local map = vim.keymap.set
-    local opts = { noremap = true, silent = true }
-    local builtin = require("telescope.builtin")
-
-    map("n", "<leader>ff", builtin.find_files, opts)
-    map("n", "<leader>fg", builtin.live_grep, opts)
-    map("n", "<leader>fb", builtin.buffers, opts)
-    map("n", "<leader>fh", builtin.help_tags, opts)
-
-    map("n", "<leader>gc", builtin.git_commits,  { desc = "Git commits" })
-    map("n", "<leader>gC", builtin.git_bcommits, { desc = "Git commits (file)" })
-    map("n", "<leader>gb", builtin.git_branches, { desc = "Git branches" })
-    map("n", "<leader>gs", builtin.git_status,   { desc = "Git status" })
-    map("n", "<leader>gS", builtin.git_stash,    { desc = "Git stash" })
-
-    map("n", "<leader>mm", ":set modifiable<CR>", opts)
-  '';
-};
 
 programs.fastfetch.enable = true;
 
