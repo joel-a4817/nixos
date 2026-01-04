38c430329af45373ffc80a434471800482d738d0
rt4817 <joel.ag789@gmail.com>
2026-01-04 19:51:16 +0530

    Yup i did it
    both vim and nvim now. I really could never choose so i guess i'll just
    keep both even if i never use vim.

--- configuration.nix
+++ configuration.nix
@@ -110,9 +110,65 @@ environment.systemPackages = with pkgs; [
   grim slurp wf-recorder
   pulseaudio brightnessctl
   imv mpv unzip zip 
-  clipse wl-clipboard xclip # (vim clipboard in xwayland)
+  clipse wl-clipboard
   appimage-run
 ];
+
+  programs.vim = {
+    enable = true;
+    extraConfig = ''
+      " --- Leader key ---
+      let mapleader=" "
+
+      " --- UI ---
+      set number
+      set relativenumber
+      set termguicolors
+      set signcolumn=auto
+
+      " Enable syntax and a simple built-in colorscheme
+      set termguicolors
+      syntax on
+      colorscheme desert
+
+      " Transparent background (inherit terminal bg), do NOT change foregrounds
+      highlight Normal guibg=NONE ctermbg=NONE
+      highlight NormalNC guibg=NONE ctermbg=NONE
+      highlight NonText guibg=NONE ctermbg=NONE
+
+      " Gutter/columns: transparent bg, keep readable numbers
+      highlight LineNr guibg=NONE ctermbg=NONE guifg=#c8c8c8 ctermfg=White
+      highlight CursorLineNr guibg=NONE ctermbg=NONE guifg=#ffffff ctermfg=White
+      highlight SignColumn guibg=NONE ctermbg=NONE
+      highlight FoldColumn guibg=NONE ctermbg=NONE
+
+      " --- Indentation ---
+      set expandtab
+      set shiftwidth=2
+      set tabstop=2
+
+      " --- Mouse
+      set mouse=a
+
+      " --- Delete without copying (black-hole) ---
+      nnoremap d  "_d
+      xnoremap d  "_d
+      nnoremap dd "_dd
+
+      " --- Cut entire line to system clipboard (dx) ---
+      nnoremap dx "+dd
+
+      " --- Sane defaults ---
+      set ttyfast
+      set incsearch
+      set hlsearch
+      set ignorecase
+      set smartcase
+      set hidden
+      set noswapfile
+      set backspace=indent,eol,start
+    '';
+  };
 
   #warp  
   services.cloudflare-warp = {
