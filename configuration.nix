090ce2525623d2c0f49541b08db3dd89b5895e8d
rt4817 <joel.ag789@gmail.com>
2026-02-02 00:49:51 +1100

    wlr enable.
    idk if this has to do with anything i just did it though

--- configuration.nix
+++ configuration.nix
@@ -203,6 +203,7 @@ in
   # xdg portal enabling
   xdg.portal = {
     enable = true;
+    wlr.enable = true;
     extraPortals = with pkgs; [
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
