1a0bb6227ddcd5c43867b63bf44af4fc4e3d28c7
gs4817 <joel.ag789@gmail.com>
2026-02-24 10:28:48 +0000

    sunshine is now pinned to the last working

--- flake.nix
+++ flake.nix
@@ -2,12 +2,14 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # Sunshine pinned to the known-good revision from your flake.lock
+    nixpkgs-sunshine.url = "github:NixOS/nixpkgs/ed142ab1b3a092c4d149245d0c4126a5d7ea00b0";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # Yazi flake (for yazi.packages.${system}.default)
     yazi.url = "github:sxyazi/yazi";
 
     solaar = {
@@ -15,23 +17,32 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  outputs = { self, nixpkgs, home-manager, solaar, yazi, ... }: {
+
+  outputs = { self, nixpkgs, nixpkgs-sunshine, home-manager, solaar, yazi, ... }: {
     nixosConfigurations.rt4817 = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
+
       modules = [
 
-        # Solaar module
         solaar.nixosModules.default
 
         ({ pkgs, ... }: {
-          nixpkgs.overlays = [ yazi.overlays.default ];
+          nixpkgs.overlays = [
+            yazi.overlays.default
+
+            # ⭐ Sunshine stays on ed142ab1… (yesterday's working version)
+            (final: prev: {
+              sunshine = (import nixpkgs-sunshine {
+                system = prev.system;
+              }).sunshine;
+            })
+          ];
         })
 
         ./configuration.nix
 
-        # Home Manager
         home-manager.nixosModules.home-manager
-        ({ ... }: {
+        ({
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.users.joel = { pkgs, ... }: {
