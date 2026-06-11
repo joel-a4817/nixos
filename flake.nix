fdf2afb58a0a1e6731e0f6b71e6b4aef1ba6cb9a
rt4817 <joel.ag789@gmail.com>
2026-06-11 21:26:07 +1000

    added vieb

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    vieb-nix.url = "github:tejing1/vieb-nix";
+    vieb-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -15,7 +17,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, solaar, yazi, ... }:
+  outputs = { self, nixpkgs, home-manager, solaar, yazi, vieb-nix, ... }:
   let
     system = "x86_64-linux";
 
@@ -26,19 +28,17 @@
   {
     nixosConfigurations.rt4817 = nixpkgs.lib.nixosSystem {
       inherit system;
-
       modules = [
         solaar.nixosModules.default
-
         ({ ... }: { nixpkgs.overlays = overlays; })
-
         ./configuration.nix
-
+        ({pkgs, ...}: {
+          environment.systemPackages = [ (vieb-nix.packagesFunc pkgs).vieb ];
+        })
         home-manager.nixosModules.home-manager
         ({ ... }: {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-
           home-manager.users.joel = import ./home.nix;
         })
       ];
