1521eea5bd501c038651aa0be520c4adc0258145
rt4817 <joel.ag789@gmail.com>
2026-07-18 23:07:35 +1000

    changed wlr settings

--- rt4817/display.nix
+++ rt4817/display.nix
@@ -37,12 +37,5 @@
       xdg-desktop-portal-wlr
       xdg-desktop-portal-gtk
     ];
-    config = {
-      common = {
-        default = [ "gtk" ];
-        "org.freedesktop.impl.portal.Screenshot" = [ "wlr" ];
-        "org.freedesktop.impl.portal.ScreenCast" = [ "wlr" ];
-      };
-    };
   };
 }
