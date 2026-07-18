2468ccfd8d6d74a2b2f8632452b831705aaa5303
rt4817 <joel.ag789@gmail.com>
2026-07-18 11:50:15 +1000

    added wvkbd again

--- joel/packages.nix
+++ joel/packages.nix
@@ -7,13 +7,12 @@
     anki
     heimdall-gui android-tools
     kdePackages.kamoso
-    clipse calcurse
+    clipse calcurse wvkbd
     collabora-desktop
     discord signal-desktop karere
     prismlauncher kicad bambu-studio opencv
     qt6.qtwayland #Required for Qt apps like those above.
-    #yazi pkgs:
-    ffmpeg p7zip jq fzf zoxide resvg imagemagick
+    ffmpeg p7zip jq fzf zoxide resvg imagemagick #yazi pkgs
     trash-cli lazygit fd ripgrep nushell #required by yazi plugins
   ];
 }
