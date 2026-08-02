04fee0c1e90a8d3876f78528476390dcabfbc910
rt4817 <joel.ag789@gmail.com>
2026-08-02 22:33:44 +1000

    fixed yazi sudo plugin

--- home-pkgs.nix
+++ home-pkgs.nix
@@ -1,5 +1,22 @@
 { config, pkgs, lib, ... }:
 
+let
+  sudo-yazi-fixed = pkgs.yaziPlugins.sudo.overrideAttrs (old: {
+    postPatch = (old.postPatch or "") + ''
+      plugin_file="$(find . -name main.lua -print -quit)"
+
+      substituteInPlace "$plugin_file" \
+        --replace-fail \
+          'for _, url in pairs(cx.yanked) do' \
+          'for _, file in pairs(cx.yanked) do'
+
+      substituteInPlace "$plugin_file" \
+        --replace-fail \
+          'table.insert(yanked, tostring(url))' \
+          'table.insert(yanked, tostring(file.url))'
+    '';
+  });
+in
 {
   home.packages = with pkgs; [
     localsend 
@@ -18,7 +35,7 @@
     shellWrapperName = "y";
     package = pkgs.yazi.override { _7zz = pkgs._7zz-rar; }; # Support for RAR extraction
     plugins = {
-      sudo = pkgs.yaziPlugins.sudo; #https://github.com/TD-Sky/sudo.yazi
+      sudo = sudo-yazi-fixed; #https://github.com/TD-Sky/sudo.yazi
       lazygit = pkgs.yaziPlugins.lazygit; #https://github.com/Lil-Dank/lazygit.yazi
       recycle-bin = pkgs.yaziPlugins.recycle-bin; #https://github.com/uhs-robert/recycle-bin.yazi
       restore = pkgs.yaziPlugins.restore; #https://github.com/boydaihungst/restore.yazi
