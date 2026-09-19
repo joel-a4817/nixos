2eb289547ac6c85206a885cbbbe4868740f1603c
rt4817 <joel.ag789@gmail.com>
2026-09-19 22:31:19 +1000

    removed unnecessary systemctl from sudo

--- rt4817/users.nix
+++ rt4817/users.nix
@@ -21,8 +21,6 @@ security.sudo.extraRules = [
       { command = "/run/current-system/sw/bin/pkill"; options = [ "NOPASSWD" ]; }
       { command = "/run/current-system/sw/bin/setsid"; options = [ "NOPASSWD" ]; }
       { command = "/home/joel/.config/sway/scripts/rotate-touchpad.py"; options = [ "NOPASSWD" ]; }
-      { command = "/run/current-system/sw/bin/systemctl restart nqptp.service"; options = [ "NOPASSWD" ]; }
-      { command = "/run/current-system/sw/bin/systemctl restart shairport-sync.service"; options = [ "NOPASSWD" ]; }
     ];
   }
 ];
