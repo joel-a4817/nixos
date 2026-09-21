df09e21348ffc4fc17e3a7a14139e51081405426
rt4817 <joel.ag789@gmail.com>
2026-09-21 23:15:23 +1000

    pipewire update script to sway

--- rt4817/pipewire.nix
+++ rt4817/pipewire.nix
@@ -131,7 +131,116 @@
       ];
     };
 
-    extraConfig.pipewire."01-earpods-0737ms-spesbourg-castle-main-building-v2" = {
+    extraConfig.pipewire."01-cloud3-0000ms-anechoic-oe" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-filter-chain";
+
+          args = {
+            "node.description" = "cloud3 - (0000ms) Anechoic (OE)";
+
+            "filter.graph" = {
+              nodes = [
+                { type = "builtin"; label = "copy"; name = "splitL"; }
+                { type = "builtin"; label = "copy"; name = "splitR"; }
+
+                {
+                  type = "builtin";
+                  label = "convolver";
+                  name = "LL";
+                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 0; };
+                }
+                {
+                  type = "builtin";
+                  label = "convolver";
+                  name = "LR";
+                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 1; };
+                }
+                {
+                  type = "builtin";
+                  label = "convolver";
+                  name = "RL";
+                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 2; };
+                }
+                {
+                  type = "builtin";
+                  label = "convolver";
+                  name = "RR";
+                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 3; };
+                }
+
+                {
+                  type = "builtin";
+                  label = "mixer";
+                  name = "mixL";
+                  control = {
+                    "Gain 1" = 0.11091748152624009;
+                    "Gain 2" = 0.11091748152624009;
+                  };
+                }
+                {
+                  type = "builtin";
+                  label = "mixer";
+                  name = "mixR";
+                  control = {
+                    "Gain 1" = 0.11091748152624009;
+                    "Gain 2" = 0.11091748152624009;
+                  };
+                }
+
+                {
+                  type = "builtin";
+                  label = "convolver";
+                  name = "hpcfL";
+                  config = { filename = "/home/joel/Documents/prefs/audio/HyperX_Cloud_III_Average.wav"; channel = 0; };
+                }
+                {
+                  type = "builtin";
+                  label = "convolver";
+                  name = "hpcfR";
+                  config = { filename = "/home/joel/Documents/prefs/audio/HyperX_Cloud_III_Average.wav"; channel = 1; };
+                }
+              ];
+
+              links = [
+                { output = "splitL:Out"; input = "LL:In"; }
+                { output = "splitL:Out"; input = "LR:In"; }
+                { output = "splitR:Out"; input = "RL:In"; }
+                { output = "splitR:Out"; input = "RR:In"; }
+                { output = "LL:Out"; input = "mixL:In 1"; }
+                { output = "RL:Out"; input = "mixL:In 2"; }
+                { output = "LR:Out"; input = "mixR:In 1"; }
+                { output = "RR:Out"; input = "mixR:In 2"; }
+                { output = "mixL:Out"; input = "hpcfL:In"; }
+                { output = "mixR:Out"; input = "hpcfR:In"; }
+              ];
+
+              inputs = [ "splitL:In" "splitR:In" ];
+              outputs = [ "hpcfL:Out" "hpcfR:Out" ];
+            };
+
+            "capture.props" = {
+              "node.name" = "cloud3_0000ms-anechoic-oe";
+              "node.description" = "cloud3 - (0000ms) Anechoic (OE)";
+              "media.class" = "Audio/Sink";
+              "audio.channels" = 2;
+              "audio.position" = [ "FL" "FR" ];
+              "audio.rate" = 96000;
+              "stream.dont-remix" = true;
+            };
+
+            "playback.props" = {
+              "audio.channels" = 2;
+              "audio.position" = [ "FL" "FR" ];
+              "audio.rate" = 96000;
+              "stream.dont-remix" = true;
+            };
+          };
+        }
+      ];
+    };
+
+    extraConfig.pipewire."02-earpods-0737ms-spesbourg-castle-main-building-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -240,7 +349,7 @@
       ];
     };
 
-    extraConfig.pipewire."02-earpods-0775ms-elveden-hall-suffolk-england-v2" = {
+    extraConfig.pipewire."03-earpods-0775ms-elveden-hall-suffolk-england-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -349,7 +458,7 @@
       ];
     };
 
-    extraConfig.pipewire."03-earpods-0777ms-hoffmann-lime-kiln-langcliffe-uk" = {
+    extraConfig.pipewire."04-earpods-0777ms-hoffmann-lime-kiln-langcliffe-uk" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -458,7 +567,7 @@
       ];
     };
 
-    extraConfig.pipewire."04-earpods-0840ms-laubenheim-barn-v2" = {
+    extraConfig.pipewire."05-earpods-0840ms-laubenheim-barn-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -567,7 +676,7 @@
       ];
     };
 
-    extraConfig.pipewire."05-earpods-1199ms-abies-grandis-forest-wheldrake-wood" = {
+    extraConfig.pipewire."06-earpods-1199ms-abies-grandis-forest-wheldrake-wood" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -676,7 +785,7 @@
       ];
     };
 
-    extraConfig.pipewire."06-earpods-1223ms-les-dominicains-de-haute-alsace-neo-gothic-chapel-v2" = {
+    extraConfig.pipewire."07-earpods-1223ms-les-dominicains-de-haute-alsace-neo-gothic-chapel-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -785,7 +894,7 @@
       ];
     };
 
-    extraConfig.pipewire."07-earpods-1285ms-small-hall-of-the-konzerthaus-berlin" = {
+    extraConfig.pipewire."08-earpods-1285ms-small-hall-of-the-konzerthaus-berlin" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -894,7 +1003,7 @@
       ];
     };
 
-    extraConfig.pipewire."08-earpods-1334ms-strasbourg-observatory-v2" = {
+    extraConfig.pipewire."09-earpods-1334ms-strasbourg-observatory-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1003,7 +1112,7 @@
       ];
     };
 
-    extraConfig.pipewire."09-earpods-1343ms-singer-polignac-foundation-music-salon-v2" = {
+    extraConfig.pipewire."10-earpods-1343ms-singer-polignac-foundation-music-salon-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1112,7 +1221,7 @@
       ];
     };
 
-    extraConfig.pipewire."10-earpods-1345ms-falkland-palace-royal-tennis-court-v3" = {
+    extraConfig.pipewire."11-earpods-1345ms-falkland-palace-royal-tennis-court-v3" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1221,7 +1330,7 @@
       ];
     };
 
-    extraConfig.pipewire."11-earpods-1347ms-athenee-theatre-main-hall-v2" = {
+    extraConfig.pipewire."12-earpods-1347ms-athenee-theatre-main-hall-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1330,7 +1439,7 @@
       ];
     };
 
-    extraConfig.pipewire."12-earpods-1426ms-karlsruhe-state-theatre-main-hall-v2" = {
+    extraConfig.pipewire."13-earpods-1426ms-karlsruhe-state-theatre-main-hall-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1439,7 +1548,7 @@
       ];
     };
 
-    extraConfig.pipewire."13-earpods-1443ms-detmold-konzerthaus-medium-sized-concert-hall-600-seats" = {
+    extraConfig.pipewire."14-earpods-1443ms-detmold-konzerthaus-medium-sized-concert-hall-600-seats" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1548,7 +1657,7 @@
       ];
     };
 
-    extraConfig.pipewire."14-earpods-1531ms-tyndall-bruce-monument-v2" = {
+    extraConfig.pipewire."15-earpods-1531ms-tyndall-bruce-monument-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1657,7 +1766,7 @@
       ];
     };
 
-    extraConfig.pipewire."15-earpods-1578ms-usina-del-arte-symphony-hall-v2" = {
+    extraConfig.pipewire."16-earpods-1578ms-usina-del-arte-symphony-hall-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1766,7 +1875,7 @@
       ];
     };
 
-    extraConfig.pipewire."16-earpods-1599ms-main-auditorium-of-the-finnish-national-opera-and-ballet-fnob" = {
+    extraConfig.pipewire."17-earpods-1599ms-main-auditorium-of-the-finnish-national-opera-and-ballet-fnob" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1875,7 +1984,7 @@
       ];
     };
 
-    extraConfig.pipewire."17-earpods-1806ms-zkm-karlsruhe-mezzanine-rear-v2" = {
+    extraConfig.pipewire."18-earpods-1806ms-zkm-karlsruhe-mezzanine-rear-v2" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -1984,7 +2093,7 @@
       ];
     };
 
-    extraConfig.pipewire."18-earpods-2264ms-promenadikeskus-concert-hall-in-pori-finland" = {
+    extraConfig.pipewire."19-earpods-2264ms-promenadikeskus-concert-hall-in-pori-finland" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -2093,7 +2202,7 @@
       ];
     };
 
-    extraConfig.pipewire."19-earpods-4400ms-york-minsters-chapter-house" = {
+    extraConfig.pipewire."20-earpods-4400ms-york-minsters-chapter-house" = {
       "context.modules" = [
         {
           name = "libpipewire-module-filter-chain";
@@ -2202,113 +2311,46 @@
       ];
     };
 
-    extraConfig.pipewire."20-cloud3-0000ms-anechoic-oe" = {
-      "context.modules" = [
-        {
-          name = "libpipewire-module-filter-chain";
 
-          args = {
-            "node.description" = "cloud3 - (0000ms) Anechoic (OE)";
 
-            "filter.graph" = {
-              nodes = [
-                { type = "builtin"; label = "copy"; name = "splitL"; }
-                { type = "builtin"; label = "copy"; name = "splitR"; }
 
-                {
-                  type = "builtin";
-                  label = "convolver";
-                  name = "LL";
-                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 0; };
-                }
-                {
-                  type = "builtin";
-                  label = "convolver";
-                  name = "LR";
-                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 1; };
-                }
-                {
-                  type = "builtin";
-                  label = "convolver";
-                  name = "RL";
-                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 2; };
-                }
-                {
-                  type = "builtin";
-                  label = "convolver";
-                  name = "RR";
-                  config = { filename = "/home/joel/Documents/prefs/audio/BRIRs/(0000ms) Anechoic (OE)/BRIR_True_Stereo.wav"; channel = 3; };
-                }
 
-                {
-                  type = "builtin";
-                  label = "mixer";
-                  name = "mixL";
-                  control = {
-                    "Gain 1" = 0.11091748152624009;
-                    "Gain 2" = 0.11091748152624009;
-                  };
-                }
-                {
-                  type = "builtin";
-                  label = "mixer";
-                  name = "mixR";
-                  control = {
-                    "Gain 1" = 0.11091748152624009;
-                    "Gain 2" = 0.11091748152624009;
-                  };
-                }
 
-                {
-                  type = "builtin";
-                  label = "convolver";
-                  name = "hpcfL";
-                  config = { filename = "/home/joel/Documents/prefs/audio/HyperX_Cloud_III_Average.wav"; channel = 0; };
-                }
-                {
-                  type = "builtin";
-                  label = "convolver";
-                  name = "hpcfR";
-                  config = { filename = "/home/joel/Documents/prefs/audio/HyperX_Cloud_III_Average.wav"; channel = 1; };
-                }
-              ];
 
-              links = [
-                { output = "splitL:Out"; input = "LL:In"; }
-                { output = "splitL:Out"; input = "LR:In"; }
-                { output = "splitR:Out"; input = "RL:In"; }
-                { output = "splitR:Out"; input = "RR:In"; }
-                { output = "LL:Out"; input = "mixL:In 1"; }
-                { output = "RL:Out"; input = "mixL:In 2"; }
-                { output = "LR:Out"; input = "mixR:In 1"; }
-                { output = "RR:Out"; input = "mixR:In 2"; }
-                { output = "mixL:Out"; input = "hpcfL:In"; }
-                { output = "mixR:Out"; input = "hpcfR:In"; }
-              ];
 
-              inputs = [ "splitL:In" "splitR:In" ];
-              outputs = [ "hpcfL:Out" "hpcfR:Out" ];
-            };
 
-            "capture.props" = {
-              "node.name" = "cloud3_0000ms-anechoic-oe";
-              "node.description" = "cloud3 - (0000ms) Anechoic (OE)";
-              "media.class" = "Audio/Sink";
-              "audio.channels" = 2;
-              "audio.position" = [ "FL" "FR" ];
-              "audio.rate" = 96000;
-              "stream.dont-remix" = true;
-            };
 
-            "playback.props" = {
-              "audio.channels" = 2;
-              "audio.position" = [ "FL" "FR" ];
-              "audio.rate" = 96000;
-              "stream.dont-remix" = true;
-            };
-          };
-        }
-      ];
-    };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   };
 }
